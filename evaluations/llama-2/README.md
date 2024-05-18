05dac50d076948cb55fe07f1c6ae33959228cd53
Finn Woelm <7606194+FinnWoelm@users.noreply.github.com>
2024-05-18 17:52:27 +0800

    Benchmark models against SDG 8

--- evaluations/llama-2/README.md
+++ evaluations/llama-2/README.md
@@ -16,7 +16,7 @@ System Prompt:
 ```
 You are an intelligent multi-label classification system designed to map texts to their relevant Sustainable Development Goals.
 
-Take the text delimited by triple quotation marks and return a JSON list of relevant SDGs. Example: {"sdgs": [1, 6, 14]}
+Take the text delimited by triple quotation marks and return a JSON list of relevant SDGs in a single line. Example: {"sdgs": [1, 6, 14]}
 ```
 
 Prompt:
@@ -32,18 +32,19 @@ Learn more: https://llama.meta.com/llama2/
 
 ## Evaluation
 
-| SDG     |    n |   Accuracy (%) |   Precision (%) |   Recall (%) |   F1 Score |   TP |   FP |   TN |   FN |
-|:--------|-----:|---------------:|----------------:|-------------:|-----------:|-----:|-----:|-----:|-----:|
-| Average | 77.4 |           88.0 |            85.3 |         93.0 |       0.88 | 35.4 |  6.2 |   33 |  2.8 |
-| 1       |   77 |           88.3 |            78.1 |         92.6 |       0.85 |   25 |    7 |   43 |    2 |
-| 2       |   69 |           84.1 |            97.2 |         77.8 |       0.86 |   35 |    1 |   23 |   10 |
-| 3       |   76 |           93.4 |            84.8 |        100.0 |       0.92 |   28 |    5 |   43 |    0 |
-| 4       |   82 |           90.2 |            84.3 |        100.0 |       0.91 |   43 |    8 |   31 |    0 |
-| 5       |   69 |           92.8 |            96.9 |         88.6 |       0.93 |   31 |    1 |   33 |    4 |
-| 6       |   85 |           84.7 |            80.7 |         95.8 |       0.88 |   46 |   11 |   26 |    2 |
-| 7       |  100 |           92.0 |            88.9 |         96.0 |       0.92 |   48 |    6 |   44 |    2 |
-| 10      |   61 |           78.7 |            71.1 |         93.1 |       0.81 |   27 |   11 |   21 |    2 |
+| SDG     |   n |   Accuracy (%) |   Precision (%) |   Recall (%) |   F1 Score |   TP |   FP |   TN |   FN |
+|:--------|----:|---------------:|----------------:|-------------:|-----------:|-----:|-----:|-----:|-----:|
+| Average |  77 |           87.3 |            84.4 |         93.0 |       0.88 | 35.3 |  6.7 | 32.2 |  2.8 |
+| 1       |  77 |           89.6 |            80.6 |         92.6 |       0.86 |   25 |    6 |   44 |    2 |
+| 2       |  69 |           82.6 |            97.1 |         75.6 |       0.85 |   34 |    1 |   23 |   11 |
+| 3       |  76 |           93.4 |            84.8 |        100.0 |       0.92 |   28 |    5 |   43 |    0 |
+| 4       |  82 |           90.2 |            84.3 |        100.0 |       0.91 |   43 |    8 |   31 |    0 |
+| 5       |  69 |           92.8 |            94.1 |         91.4 |       0.93 |   32 |    2 |   32 |    3 |
+| 6       |  85 |           87.1 |            83.6 |         95.8 |       0.89 |   46 |    9 |   28 |    2 |
+| 7       | 100 |           93.0 |            90.6 |         96.0 |       0.93 |   48 |    5 |   45 |    2 |
+| 8       |  74 |           78.4 |            73.9 |         89.5 |       0.81 |   34 |   12 |   24 |    4 |
+| 10      |  61 |           78.7 |            70.0 |         96.6 |       0.81 |   28 |   12 |   20 |    1 |
 
-**Benchmarked on**: May 10, 2024
+**Benchmarked on**: May 18, 2024
 
 **Detailed benchmark results**: [results.csv](results.csv)
--- evaluations/llama-2/llama-2.py
+++ evaluations/llama-2/llama-2.py
@@ -13,7 +13,34 @@ class Classifier(BaseClassifier):
     def __post_init__(self) -> None:
         self.replicate_run = self.cache.memoize()(replicate.run)
 
+        # text = "Two thirds of this support went towards reducing emissions from and improving the resilience of energy, transport and water infrastructure. In support of this, a Climate Change Financing Framework was developed in 2014 to outline the government's plans for mobilising resources to support its NDC. Much of the international climate finance received by Cambodia has focused on providing project level support. The limitations of such approaches include a disaggregation of support across various areas, especially in areas where donor coordination and harmonisation are poor, and reduced alignment with national priorities."
+
+        # # key = self.replicate_run.__cache_key__(
+        # #     "meta/llama-2-70b-chat",
+        # #     input=dict(
+        # #         prompt="\n\n".join(
+        # #             [
+        # #                 "Classify the following text in terms of its relevance to the Sustainable Development Goals:",
+        # #                 f'Text: """{text}"""',
+        # #             ]
+        # #         ),
+        # #         system_prompt="\n\n".join(
+        # #             [
+        # #                 "You are an intelligent multi-label classification system designed to map texts to their relevant Sustainable Development Goals.",
+        # #                 'Take the text delimited by triple quotation marks and return a JSON list of relevant SDGs. Example: {"sdgs": [1, 6, 14]}',
+        # #             ]
+        # #         ),
+        # #         # Stop after the first line: Llama2 tends to add explanations
+        # #         # for the chosen SDGs after the first line
+        # #         stop_sequences="\n",
+        # #         max_tokens=100,
+        # #         temperature=0.2,
+        # #     ),
+        # # )
+        # # self.cache.delete(key)
+
     def predict_sdgs(self, text: str) -> list[int]:
+        print(text)
         response = self.replicate_run(
             "meta/llama-2-70b-chat",
             input=dict(
@@ -26,18 +53,27 @@ class Classifier(BaseClassifier):
                 system_prompt="\n\n".join(
                     [
                         "You are an intelligent multi-label classification system designed to map texts to their relevant Sustainable Development Goals.",
-                        'Take the text delimited by triple quotation marks and return a JSON list of relevant SDGs. Example: {"sdgs": [1, 6, 14]}',
+                        'Take the text delimited by triple quotation marks and return a JSON list of relevant SDGs in a single line. Example: {"sdgs": [1, 6, 14]}',
                     ]
                 ),
                 # Stop after the first line: Llama2 tends to add explanations
                 # for the chosen SDGs after the first line
                 stop_sequences="\n",
                 max_tokens=100,
-                temperature=0.2,
+                temperature=0,
             ),
         )
 
+        # Combine tokens
+        # message = "".join(response) + "}"
+
+        # # Keep only the text up to the closing bracket
+        # message = message[: message.find("}") + 1]
+
+        # Squish text
+
         message = "".join(response)
+        print(message)
 
         # Verify that message is not empty
         if not message:
